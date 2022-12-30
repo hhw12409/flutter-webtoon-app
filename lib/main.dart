6133d340d59318cf5b01b36fdc4403728ede1bdc
hhw12409 <sksk2425@naver.com>
2022-12-31 01:33:10 +0900

    feat: make home_screen and appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Text('Hello world'),
+        body: HomeScreen(),
       ),
     );
   }
