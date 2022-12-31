e9e24eddd3002614f9984c415bc751d39302ba9b
hhw12409 <sksk2425@naver.com>
2022-12-31 18:34:46 +0900

    feat: add api in home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon_app/models/webtoon_model.dart';
 import 'package:webtoon_app/screens/home_screen.dart';
 import 'package:webtoon_app/services/api_service.dart';
 
 void main() {
-  ApiService().getTodayToons();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebtoon() async {
+    webtoons = await ApiService.getTodayToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebtoon();
+  }
 
   @override
   Widget build(BuildContext context) {
