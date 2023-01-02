2898b044045b4898e70bee496d45c380056f51a7
hhw12409 <sksk2425@naver.com>
2023-01-03 01:14:00 +0900

    feat: WebtoonEpisode api

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:webtoon_app/models/webtoon_detail_model.dart';
+import 'package:webtoon_app/models/webtoon_episode_model.dart';
 import 'package:webtoon_app/models/webtoon_model.dart';
 
 class ApiService {
@@ -30,6 +31,16 @@ class ApiService {
     if (response.statusCode == 200) {
       var webtoon = jsonDecode(response.body);
       return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonEpisodeModel> getToonEpisodeById(String id) async {
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      var webtoon = jsonDecode(response.body);
+      return WebtoonEpisodeModel.fromJson(webtoon);
     }
     throw Error();
   }
