746e3862d5f150318f6c919aca105114b9b1aee3
hhw12409 <sksk2425@naver.com>
2023-01-04 01:05:09 +0900

    feat: add genre and age

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -65,9 +65,35 @@ class _DetailScreenState extends State<DetailScreen> {
             future: webtoon,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return Text(snapshot.data!.about);
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "${snapshot.data!.genre} | ${snapshot.data!.age}",
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      )
+                    ],
+                  ),
+                );
               }
-              return const Text('...');
+              return const CircularProgressIndicator(
+                color: Colors.black,
+              );
             },
           ),
         ],
