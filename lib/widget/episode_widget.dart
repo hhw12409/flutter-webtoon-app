8965a6a5c3a84c97b637f70ca7736c3526e3dc08
hhw12409 <sksk2425@naver.com>
2023-01-05 01:50:52 +0900

    feat: episode link function

--- lib/widget/episode_widget.dart
+++ lib/widget/episode_widget.dart
@@ -6,45 +6,58 @@ class Episode extends StatelessWidget {
   const Episode({
     Key? key,
     required this.episode,
+    required this.webtoonId,
   }) : super(key: key);
 
+  final String webtoonId;
   final WebtoonEpisodeModel episode;
 
   Future onButtonTap() async {
-    final url = Uri.parse('https://google.com');
-    await launchUrl(url);
+    final url = Uri.parse(
+        'https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episode.id}');
+    return await launchUrl(url);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 10),
-      decoration: BoxDecoration(
-        color: Colors.green.shade400,
-        borderRadius: BorderRadius.circular(
-          20,
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 10,
-          horizontal: 20,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              episode.title,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-              ),
-            ),
-            const Icon(
-              Icons.chevron_right_outlined,
-              color: Colors.white,
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        decoration: BoxDecoration(
+          color: Colors.green.shade400,
+          borderRadius: BorderRadius.circular(
+            20,
+          ),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 2,
+              offset: const Offset(2, 2),
+              color: Colors.black.withOpacity(0.1),
             ),
           ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 20,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              const Icon(
+                Icons.chevron_right_outlined,
+                color: Colors.white,
+              ),
+            ],
+          ),
         ),
       ),
     );
